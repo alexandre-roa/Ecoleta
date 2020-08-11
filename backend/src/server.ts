d1b0168326d96823585312cf48bc0af76e19d94f
Alexandre <alex_roa@aol.com>
2020-08-11 16:20:06 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+// GET = Obter uma informação
+// POST = Criar uma informação
+// PUT = Atualizar um informação já existente
+// DELETE = Deletar uma informação já existente
+
+app.get('/users', (request, response) => {
+  const dbArray = ['Alexandre', 'Diego', 'Maria', 'Ana'];
+
+  response.json(dbArray);
+});
+
+app.listen(8080);
+console.log('server is running on port 8080');
