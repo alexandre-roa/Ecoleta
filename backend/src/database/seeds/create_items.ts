e5bdbda8b63a6d18ba5a88c8ae2a5f36159249a4
Alexandre <alex_roa@aol.com>
2020-08-11 22:17:06 -0300

    backend 0.7

---
+++ backend/src/database/seeds/create_items.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex';
+
+export async function seed(knex: Knex) {
+  await knex('items').insert([
+    { title: 'Lâmpadas', image: 'lampadas.svg' },
+    { title: 'Pilhas e baterias', image: 'baterias.svg' },
+    { title: 'Papeis e papelão', image: 'papeis-papelao.svg' },
+    { title: 'Residuos eletrônicos', image: 'eletronicos.svg' },
+    { title: 'Residuos orgânicos', image: 'organicos.svg' },
+    { title: 'Oleo de cozinha', image: 'oleo.svg' }
+  ]);
+}
